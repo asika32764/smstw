ed8d7912fe7bfc209c0a564c9a4baa7f88477f12
Asika <asika32764@gmail.com>
2013-08-27 00:51:18 +0800

    skrollr added & create SMS object

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
  * SMS Taiwan CO. LTD.
  */
 
-var debug = true ;
+var debug = false ;
 
 less = {
     env: "development", // or "production"
@@ -22,139 +22,172 @@ less = {
                         //resource
 };
 
-// Scroll Spy
-window.onload = function() {
-    $('[sr-scrollspy]').each(function(i, e){
-        var id = $(e).attr('id');
-        
-        $(e).scrollspy({
-            min: $(e).offset().top,
-            max: $(e).offset().top + 100,
-            onEnter: function(element, position) {
-                var navs    = $('#sr-nav li');
-                var target  = navs.find('a[href=#'+id+']');
-                
-                navs.removeClass('uk-active');
-                target.parent().addClass('uk-active');
-                
-                if (debug) console.log('in: '+id);
-            },
-            onLeave: function(element, position) {
-                if (debug) console.log('out: '+id);
-            }
-        }); 
-    });
-    
-    $('a').smoothScroll();
-    
+// Option
+var option = {
+    address : '臺北市松山區南京東路五段356號15樓之1' 
 };
 
-//$(document).ready(function() {
-//    $('[data-uk-scrollspy]').on({
-//        
-//        'uk.scrollspy.inview': function(event){
-//            var navs    = $('#sr-nav li');
-//            var id      = $(this).attr('id');
-//            var target  = navs.find('a[href=#'+id+']');
-//            
-//            navs.removeClass('uk-active');
-//            target.parent().addClass('uk-active');
-//            
-//            if (debug) console.log('in: '+id);
-//        },
-//    
-//        'uk.scrollspy.outview': function(event){
-//            var id      = $(this).attr('id');
-//            if (debug) console.log('out: '+id);
-//        }
-//    });
-//    
-//    $('#sr-nav li a').click(function(event){
-//        var li = $(this).parent();
-//        var lis = li.parent().find('li');
-//        
-//        lis.removeClass('uk-active');
-//        li.addClass('uk-active');
-//    });
-//});
+// SMS Object
+var SMS = ({
+    
+    init: function(option)
+    {
+        this.scrollspy();
+        
+        this.smoothScroll();
+        
+        this.loadGoogleMap(option.address);
+        
+        this.parallaxScroll();
+        
+        return this;
+    },
+    
+    activeNav : function(id)
+    {
+        var navs    = $('#sr-nav li');
+        var target  = navs.find('a[href=#'+id+']');
+        
+        navs.removeClass('uk-active');
+        target.parent().addClass('uk-active');
+    },
+    
+    scrollspy: function()
+    {
+        window.onload = function()
+        {
+            $('[sr-scrollspy]').each(function(i, e){
+                var id = $(e).attr('id');
+                
+                $(e).scrollspy({
+                    min: $(e).offset().top,
+                    max: $(e).offset().top + 100,
+                    onEnter: function(element, position) {
+                        SMS.activeNav(id);
+                        
+                        if (debug) console.log('in: '+id);
+                    },
+                    onLeave: function(element, position) {
+                        if (debug) console.log('out: '+id);
+                    }
+                }); 
+            });
+        };
+    },
+    
+    smoothScroll: function()
+    {
+        jQuery(document).ready(function(){
+            
+            $('a').each(function(i, e){
+                var id = $(this).attr('href').replace('#', '');
+                
+                $(this).smoothScroll({
+                    preventDefault: false,
+                    speed: 800,
+                    afterScroll: function(a, b)
+                    {
+                        SMS.activeNav(id);
+                    },
+                }); 
+            });
+            
+        });
+        
+    },
+    
+    parallaxScroll: function()
+    {
+        // Skrollr
+        jQuery(document).ready(function(){
+            var s = skrollr.init();
+        });
+        
+    },
+    
+    loadGoogleMap : function(address)
+    {
+        jQuery(document).ready(function($){
 
-
-// Google Map
-jQuery(document).ready(function($){
-
-    // Gmap3
-    $("#contact-map").gmap3({
-        map:{
-            options: {
-                center: [25.05037541867259, 121.57889975616779],//'臺北市松山區南京東路五段356號15樓之1',
-                zoom: 16,
-                mapTypeId: 'style2',
-                mapTypeControlOptions: {
-                   mapTypeIds: [google.maps.MapTypeId.ROADMAP, "style2"]
+            // Gmap3
+            $("#contact-map").gmap3({
+                map:{
+                    options: {
+                        center: [25.05037541867259, 121.57889975616779],//'臺北市松山區南京東路五段356號15樓之1',
+                        zoom: 16,
+                        mapTypeId: 'style2',
+                        mapTypeControlOptions: {
+                           mapTypeIds: [google.maps.MapTypeId.ROADMAP, "style2"]
+                        },
+                        scrollwheel: false,
+                        control: false,
+                        mapTypeControl: false,
+                        navigationControl: false,
+                        scaleControl: false,
+                        zoomControl: false,
+                        disableDefaultUI: true
+                    }
                 },
-                scrollwheel: false,
-                control: false,
-                mapTypeControl: false,
-                navigationControl: false,
-                scaleControl: false,
-                zoomControl: false,
-                disableDefaultUI: true
-            }
-        },
-        marker: {
-            values:[
-                {address:"臺北市松山區南京東路五段356號15樓之1", data:"臺北市松山區南京東路五段356號15樓之1"}
-            ],
-            events:{
-                mouseover: function(marker, event, context){
-                    var map = $(this).gmap3("get"),
-                        infowindow = $(this).gmap3({get:{name:"infowindow"}});
-                    if (infowindow){
-                        infowindow.open(map, marker);
-                        infowindow.setContent(context.data);
-                    } else {
-                        $(this).gmap3({
-                            infowindow:{
-                                anchor:marker, 
-                                options:{content: context.data}
+                marker: {
+                    values:[
+                        {address:address, data: address}
+                    ],
+                    events:{
+                        mouseover: function(marker, event, context){
+                            var map = $(this).gmap3("get"),
+                                infowindow = $(this).gmap3({get:{name:"infowindow"}});
+                            if (infowindow){
+                                infowindow.open(map, marker);
+                                infowindow.setContent(context.data);
+                            } else {
+                                $(this).gmap3({
+                                    infowindow:{
+                                        anchor:marker, 
+                                        options:{content: context.data}
+                                    }
+                                });
                             }
-                        });
+                        },
+                        mouseout: function(){
+                            var infowindow = $(this).gmap3({get:{name:"infowindow"}});
+                            if (infowindow){
+                                infowindow.close();
+                            }
+                        }
+                      }
                     }
                 },
-                mouseout: function(){
-                    var infowindow = $(this).gmap3({get:{name:"infowindow"}});
-                    if (infowindow){
-                        infowindow.close();
-                    }
-                }
-              }
-            }
-        },
-        { styledmaptype:{
-            id: "style2",
-            options:{
-                name: "Style 2"
-            },
-            styles: [
-                {
-                    stylers: [
-                        { saturation: -30 }
-                    ] 
-                },{
-                    featureType: "road",
-                    elementType: "geometry",
-                    stylers: [
-                        { lightness: 100 },
-                        { visibility: "simplified" }
+                { styledmaptype:{
+                    id: "style2",
+                    options:{
+                        name: "Style 2"
+                    },
+                    styles: [
+                        {
+                            stylers: [
+                                { saturation: -30 }
+                            ] 
+                        },{
+                            featureType: "road",
+                            elementType: "geometry",
+                            stylers: [
+                                { lightness: 100 },
+                                { visibility: "simplified" }
+                            ]
+                        },{
+                          featureType: "road",
+                          elementType: "labels",
+                          stylers: [
+                            { visibility: "simplified" }
+                          ]
+                        }
                     ]
-                },{
-                  featureType: "road",
-                  elementType: "labels",
-                  stylers: [
-                    { visibility: "simplified" }
-                  ]
                 }
-            ]
-        }
-    });
+            });
+        }); 
+    }
+}).init(option);
+
+$(window).scroll(function(){
+    //console.log($(window).scrollTop());
 });
